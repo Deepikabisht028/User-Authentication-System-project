76eff2471c1bc855a53533be0fa8d345bbd43c58
Deepikabisht028 <91360412+Deepikabisht028@users.noreply.github.com>
2023-01-26 17:10:42 +0530

    Update views.py

--- website/signup/views.py
+++ website/signup/views.py
@@ -5,20 +5,21 @@ em=''
 pwd=''
 # Create your views here.
 def signaction(request):
-    global fn,ln,s,em,pwd
+    global fn,em,pwd
     if request.method=="POST":
         m=sql.connect(host="localhost",user="root",passwd="Sam@97560",database='website')
         cursor=m.cursor()
         d=request.POST
         for key,value in d.items():
-            if key=="name":
+            if key=="first_name":
                 fn=value
             if key=="email":
                 em=value
             if key=="password":
                 pwd=value
-            c="insert into users Values('{}','{}','{}')".format(fn,em,pwd)
-            cursor.execute(c)
-            m.commit()
+        
+        c="insert into users Values('{}','{}','{}')".format(fn,em,pwd)
+        cursor.execute(c)
+        m.commit()
 
     return render(request,'signuppage.html')
